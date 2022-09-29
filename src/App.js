7abcff6f60a12e2df7703aa1b2c98fa4e8c090e3
shai9 <dennis.shakava@student.moringaschool.com>
2022-09-29 13:19:36 +0300

    Added copyright

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <NavBar />
     <MainContent />
+    <Footer />
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import Evalu8 from "../images/Evalu8.png"
 export default function NavBar(){
   return(
     <nav className='nav'>
-      <img className='img'  src= {Evalu8} alt="Evalu8Image" />
-    <h1 className='header'>Your Next Home</h1>
+      <img className='img'  src= {Evalu8} alt="Evalu8Image"  />
+       <h1 className='header'>Your Next Home</h1>
      <ul className='nav-items'>
       <li>Homes</li>
         <li>Top Picks</li>
